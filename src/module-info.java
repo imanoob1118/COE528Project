1d0684020ff9f79f516e5db2a41abe7fe30240a9
imanoob1118 <dandan.yang1118@gmail.com>
2025-03-27 16:21:21 -0400

    test

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module Bookstore {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires jdk.jsobject;
+    requires jfx.incubator.input;
+    requires jfx.incubator.richtext;
+
+    // Allow JavaFX to access classes in the 'bookstore' package
+    opens bookstore to javafx.graphics, javafx.fxml;
+    exports bookstore;
+}
